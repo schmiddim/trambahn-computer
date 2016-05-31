830e39da652d2def1ef21589c82b7a0a4e64ac6e
Michael Schmitt <schmitt.michael@funct.com>
2016-05-31 15:03:54 +0200

    done

--- show-config.php
+++ show-config.php
@@ -1,15 +1,12 @@
 <?php
 $colors = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'colors.php';
-$filterForStations = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'filterForStations.php';
-$searchForStations = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'searchForStations.php';
-
+$departures = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'departures.php';
 
 
 echo '<pre>';
 echo '==== Colors ===';
 var_dump($colors);
-echo '==== Filter for Stations ===';
-var_dump($filterForStations);
-echo '==== Search for Stations ===';
-var_dump($searchForStations);
+echo '==== Departures ===';
+var_dump($departures);
+
 echo '</pre>';
