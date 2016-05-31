3de81bcf1b650aa8615274f4af85b39b0bba7593
Michael Schmitt <schmitt.michael@funct.com>
2016-05-31 12:33:21 +0200

    config modified

--- index.php
+++ index.php
@@ -9,28 +9,28 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 
 $colors = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'colors.php';
-$filterForStations = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'filterForStations.php';
-$searchForStations = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'searchForStations.php';
-
+$departureConfig = require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'departures.php';
 
 $outputArrays = array();
 $outputArrays['lines'] = array();
-foreach ($searchForStations as $searchForStation) {
-	$searchForStation = trim($searchForStation);
-	$http = new HttpGetDepartures('http', 'www.mvg-live.de', 'ims/dfiStaticAuswahl.svc');
-	$result = $http->getDeparturesForStation($searchForStation);
-	$parser = new Departures($result);
-	$departures = $parser->getDepartures();
 
 
-	$factory = new DeparturesFactory($parser);
+foreach ($departureConfig as $departureItem) {
+    $searchForStation = trim($departureItem['station']);
+    $http = new HttpGetDepartures('http', 'www.mvg-live.de', 'ims/dfiStaticAuswahl.svc');
+    $result = $http->getDeparturesForStation($searchForStation);
+    $parser = new Departures($result);
+    $departures = $parser->getDepartures();
+    $factory = new DeparturesFactory($parser);
 
-	$lineArray = (new TrambahnComputerDepartures($factory, $colors, $filterForStations))->getOutput();
-	if (null !== $lineArray) {
-		$outputArrays['lines'][] = $lineArray;
-	}
+    $tramDepartures = new TrambahnComputerDepartures($factory, $colors, $departureItem['filterForStations']);
+    $lineArray = $tramDepartures->getOutput();
 
+    if (null !== $lineArray) {
+        $outputArrays['lines'][] = $lineArray;
+    }
 }
+
 $outputArrays['lineCount'] = count($outputArrays['lines']);
 
 //ArduinoJson Library has trouble with unicode
